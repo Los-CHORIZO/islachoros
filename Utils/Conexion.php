62faab37050ccbc79386b6603f6374091ea955e6
devatcarl-1999 <67814375+devatcarl-1999@users.noreply.github.com>
2020-11-15 16:11:02 -0300

    Add files via upload

---
+++ Utils/Conexion.php
@@ -0,0 +1,16 @@
+<?php
+
+Class Conexion{
+
+    //Atributos de la Clase para generar la conexión a la base de datos
+
+    private $server = "localhost";
+    private $user = "";
+    private $pass = "asd123";
+    
+}
+
+
+
+
+?>
